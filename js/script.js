2422734076800c3d0eacc634cb072ec89c16460c
dunzolo <rossidavide108@gmail.com>
2022-12-14 14:36:33 +0100

    milestone 0 done: create static markup

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let array = [
+    {
+        url_image:"./img/01.webp",
+        title: "Spiderman",
+        description: "Spiderman"
+    },
+    {
+        url_image:"./img/02.webp",
+        title: "Ratchet & Clank",
+        description: "Ratchet & Clank"
+    },
+    {
+        url_image:"./img/03.webp",
+        title: "Fortnite",
+        description: "Fortnite"
+    },
+    {
+        url_image:"./img/04.webp",
+        title: "Stray",
+        description: "Stray"
+    },
+    {
+        url_image:"./img/05.webp",
+        title: "Avengers",
+        description: "Avengers"
+    }
+]
