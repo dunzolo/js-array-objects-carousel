9bc64bcfcca27dc1caea9d2adb82b4a73f50ec08
dunzolo <rossidavide108@gmail.com>
2022-12-14 15:33:57 +0100

    milestone 2 done and bonus 1 done

--- js/script.js
+++ js/script.js
@@ -25,3 +25,84 @@ let array = [
         description: "Avengers"
     }
 ]
+
+const items_slider = document.querySelector('.item-slider');
+const image_left = document.querySelector('.item-left');
+
+//creiamo dinamicamente i div con le immagini del carosello
+array.forEach((elem) => {
+    items_slider.innerHTML += `
+        <div class="item">
+            <img src="${elem.url_image}">
+        </div>
+    `;
+
+    image_left.innerHTML += `
+        <div class="d-none">
+            <img src="${elem.url_image}">
+            <div class="caption">
+                <h4>${elem.title}</h4>
+                <p>${elem.description}</p>
+            </div>
+        </div>
+    `;
+});
+
+let itemActive = 0;
+
+const items = document.getElementsByClassName('item');
+items[itemActive].classList.add('active');
+
+const image = document.getElementsByClassName('d-none');
+image[itemActive].classList.add('d-block');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+next.addEventListener('click', goToNextSlides);
+
+prev.addEventListener('click', goToPreviousSlides);
+
+console.log(array.length);
+
+function goToNextSlides(){
+    if(itemActive == 0)
+    {
+        removeClassActive(itemActive);
+        itemActive = array.length - 1;
+        addClassActive(itemActive);
+    }
+    else{
+        removeClassActive(itemActive);
+        itemActive--;
+        addClassActive(itemActive);
+    }
+}
+
+function goToPreviousSlides(){
+    if(itemActive == array.length - 1)
+    {
+        removeClassActive(itemActive);
+        itemActive = 0;
+        addClassActive(itemActive);
+    }
+    else{
+        removeClassActive(itemActive);
+        itemActive++;
+        addClassActive(itemActive);
+    }
+}
+
+
+function removeClassActive(itemActive){
+    items[itemActive].classList.remove('active');
+    image[itemActive].classList.remove('d-block');
+    // circles[itemActive].classList.remove('active');
+}
+
+function addClassActive(itemActive){
+    items[itemActive].classList.add('active');
+    image[itemActive].classList.add('d-block');
+    // circles[itemActive].classList.add('active');
+}
+
